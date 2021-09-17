914f90dd5dfcbfe5fe970b0206aaa45530f95978
Gustavo Scheffer <gustavo@wine-locals.com>
2021-09-17 19:56:50 -0300

    🚧  testes

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function NotFound() {
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+        <title> Ops, não encontramos o que está procurando</title>
+      </Head>
+      <div>
+        teste
+      </div>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function Home () {
+export default function Home() {
+  const [state, setState] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`).then(res => {
+      const response = res.data
+      setState(response)
+      console.log(response)
+    })
+  }, [])
+
   return (
     <div>
-      teste
+      <input list="states" name="state" id="state" />
+      <datalist id="states">
+        {state.map(uf => {
+          return <option value={uf.nome} key={uf.id} />
+        })}
+      </datalist>
     </div>
   )
 }
